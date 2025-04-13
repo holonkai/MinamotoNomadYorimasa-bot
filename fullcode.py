9c5cd35aa9c85e60475cc34f1cde590632f721ed
rennic <160051984+gnolreve@users.noreply.github.com>
2025-04-12 23:56:53 -0700

    Update fullcode.py

--- fullcode.py
+++ fullcode.py
@@ -6,13 +6,13 @@ from datasets import load_dataset
 
 def fetch_and_clean_data():
     reddit = praw.Reddit(
-        client_id="INzdg_TV7NM6bAcQU-sWRQ",
-        client_secret="luzjBYenx5pTPKlV4lf_vjEM2VBZAw",
-        user_agent="ForumChatBot/1.0 by sobeck1001"
+        client_id=
+        client_secret=
+        user_agent=
     )
     subreddit = reddit.subreddit("UnKnoWnCheaTs")
     with open("elden_ring_cheats_data.txt", "w", encoding="utf-8") as file:
-        for post in subreddit.hot(limit=10):  # Fetch 10 posts
+        for post in subreddit.hot(limit=10):  
             file.write(post.title + " " + post.selftext + "\n")
     with open("elden_ring_cheats_data.txt", "r", encoding="utf-8") as file:
         data = file.readlines()

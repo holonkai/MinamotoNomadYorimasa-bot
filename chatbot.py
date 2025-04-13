033235353aeff4cfac2c9d6c8cd1f1d621e581fd
rennic <160051984+gnolreve@users.noreply.github.com>
2025-04-13 00:50:01 -0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -6,9 +6,9 @@ from datasets import load_dataset
 
 def fetch_and_clean_data():
     reddit = praw.Reddit(
-        client_id="INzdg_TV7NM6bAcQU-sWRQ",
-        client_secret="luzjBYenx5pTPKlV4lf_vjEM2VBZAw",
-        user_agent="ForumChatBot/1.0 by sobeck1001"
+        client_id= #i shall not disclose
+        client_secret= #i shall not disclose
+        user_agent= #i shall not disclose
     )
     subreddit = reddit.subreddit("UnKnoWnCheaTs")
     with open("elden_ring_cheats_data.txt", "w", encoding="utf-8") as file:

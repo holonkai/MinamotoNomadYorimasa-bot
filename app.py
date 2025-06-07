c0ddea8d94f11057f0bbadc66d005533bc589d96
rennic <160051984+gnolreve@users.noreply.github.com>
2025-06-07 15:10:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,12 @@ if __name__ == "__main__":
     while True:
         user_input = input("You: ")
         print("Bot:", get_response(user_input))
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+  client_id=INzdg_TV7NM6bAcQU-sWRQ
+  client_secret=luzjBYenx5pTPKlV4lf_vjEM2VBZAw
+  user_agent="ForumChatBot/1.0 by sobeck1001
